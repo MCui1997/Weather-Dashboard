29dffb933f4f0a09a834837af391dff4d84e583e
Michael Cui <cuimichael500@gmail.com>
2020-06-24 17:07:48 -0500

    Local storage

--- logic.js
+++ logic.js
@@ -1,11 +1,27 @@
+var cityList = [];
+
+
 $("#searchBtn").on("click",function(){
 
     
-
+    //Get text of the textarea
     var city = $("textarea").val();
 
-    
-    $("#city").append(city+ "<br>");
 
+    //Append to list
+    $("#city").append(city+ "<br>");
+    cityList.push(city);
+  
+    //store
+    localStorage.setItem("cities", JSON.stringify(cityList));
 
 });
+
+function getStorage(){
+
+    var test = JSON.parse(localStorage.getItem("cities"));
+
+    console.log(test);
+}
+
+getStorage();
