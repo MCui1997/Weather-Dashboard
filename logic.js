3d4d2f1935ed00b1407087824fe0c5d92cb92ca2
Michael Cui <cuimichael500@gmail.com>
2020-06-24 20:25:20 -0500

    Got Ajax to work and got data

--- logic.js
+++ logic.js
@@ -1,15 +1,31 @@
 //Initialize variables
 var cityList = [];
 
-//Call all necesarry functions
-getStorage();
-
+//Gets storage if it isn't null
+var test = JSON.parse(localStorage.getItem("cities"));
+if(test != null){
+    getStorage();
+}
 
 //Search button is pressed
 $("#searchBtn").on("click",function(){
     
     //Get text of the textarea
     var city = $("textarea").val();
+
+
+    //Only proceed forward if it is a valid city
+
+    $.ajax({
+        url: "http://api.openweathermap.org/data/2.5/weather?q="+city+",us&APPID=d2473db2d15b3f33089244526bb7a7b6",
+        type: "GET",
+        dataType: 'json',
+        data: {"data":"check"},
+        success: function(data){
+
+            console.log(data);
+        }
+      })
 
     //Append to list
     $("#city").append(city+ "<br>");
@@ -35,4 +51,12 @@ function getStorage(){
     }
     
 }
+
+//Clear city history
+    $("#clear").on("click",function(){
+
+        localStorage.clear();
+        location.reload();
+    });
+
 
