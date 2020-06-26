ba5e6202faa97e0ee1678188bae84d68d58312dd
Michael Cui <cuimichael500@gmail.com>
2020-06-25 23:14:42 -0500

    Displays temp, humidity, uvi, wind

--- logic.js
+++ logic.js
@@ -15,26 +15,89 @@ $("#searchBtn").on("click",function(){
     //clear text
     $("textarea").val("");
 
-
+    var url = "http://api.openweathermap.org/data/2.5/weather?q="+city+",us&APPID=d2473db2d15b3f33089244526bb7a7b6";
+    
+    
     //Only proceed forward if it is a valid city
 
-    $.ajax({
-        url: "http://api.openweathermap.org/data/2.5/weather?q="+city+",us&APPID=d2473db2d15b3f33089244526bb7a7b6",
-        type: "GET",
-        dataType: 'json',
-        data: {"data":"check"},
-        success: function(data){
+ 
 
-            console.log(data);
-        }
-      })
+      //Fetch the data from the website
+      fetch(url)
+        .then(function(response) {
+        
+        //Only proceed if valid city
+        if (response.ok) {
+            
+            $.ajax({
+                url: url,
+                method: "GET"
+              }).then(function(response) {
+                    
+                //temp variable
+                var tempKelvin = response.main.temp;
+                var humidity = response.main.humidity;
+                var wind = 2.23694*(response.wind.speed);
+                var icon = response.weather[0].icon;
+                var lon = response.coord.lon;
+                var lat = response.coord.lat;  
+        
 
-    //Append to list
-    $("#city").append(city+ "<br>");
-    cityList.push(city);
+                //Convert kelvings to celsius, then finally fahrenheit
+                var tempCelsius = tempKelvin - 273;
+                var tempFahrenheit = (tempCelsius * (9/5)) + 32;
+                
+                //Display values
+                $("#tempLabel").text(Math.round(tempFahrenheit)+"°F");
+                $("#humidLabel").text(humidity+"%");
+                $("#windLabel").text(Math.round(wind)+"MPH");
+                $("#iconLabel").attr("src","http://openweathermap.org/img/wn/"+icon+".png");
+                
+                //Time for second URL
+                var secondUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude={part}&appid=d2473db2d15b3f33089244526bb7a7b6";
+                
+               
+                
+                //Only proceed if valid city
+                
+                    $.ajax({
+                        url: secondUrl,
+                        method: "GET"
+                      }).then(function(data) {
+                            
+                        //temp variable
+
+                        var uvi = data.current.uvi;
+                        $("#uvLabel").text(uvi);
+                
+                  });
+                
+               
+        
+              });
+
+
+                //Capitalize 
+                city = city.toUpperCase();
+
+                //Display to html
+                $("#cityLabel").text(city);
+
+                //Append to list
+                $("#city").append(city+ "<br>");
+                cityList.push(city);
   
-    //store
-    localStorage.setItem("cities", JSON.stringify(cityList));
+                //store
+                localStorage.setItem("cities", JSON.stringify(cityList));
+
+        } else{
+
+            alert("Not a valid city");
+            return;
+        }
+        });
+
+
 
 });
 
